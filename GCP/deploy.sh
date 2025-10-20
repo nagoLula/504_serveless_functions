0e8e3b0be658f74b3d1b036a5c641ef7fce49db8
NK <nagojkh38@gmail.com>
2025-10-19 21:32:09 -0400

    Update deployment script for potassium_classifier

--- GCP/deploy.sh
+++ GCP/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-gcloud functions deploy potassium_triage \
+gcloud functions deploy potassium_classifier \
   --gen2 \
   --runtime python310 \
   --region us-central1 \
-  --entry-point potassium_triage \
+  --entry-point potassium_classifier \
   --source=. \
   --trigger-http \
-  --allow-unauthenticated
+  --allow-unauthenticated \
+  --project=serverless-functions-475623
 
 
